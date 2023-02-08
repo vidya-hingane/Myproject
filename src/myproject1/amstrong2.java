fa3351e58dc7335e25f3867b7a825e0aadf00f0f
vidya-hingane <vhingane0@gmail.com>
2023-02-08 18:48:17 +0530

    my second commit

---
+++ src/myproject1/amstrong2.java
@@ -0,0 +1,25 @@
+package myproject1;
+
+public class amstrong2 {
+
+	public static void main(String[] args) {
+		int num=153;
+		int rem;
+		int amongnum=0;
+		int temp=num;
+		while(num>0)
+		{rem=num%10;
+		amongnum=(int) (amongnum+Math.pow(rem,3 ));
+		num=num/10;
+		}
+		System.out.print(amongnum);
+		if(temp==amongnum)
+			System.out.print("number is amstrong");
+		else
+			System.out.print("number is not amstrong");
+		}
+
+
+	}
+
+
